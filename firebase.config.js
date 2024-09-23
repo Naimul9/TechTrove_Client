52479159255bc248438f6a0015eb1f7aba567457
Naimul9 <naimulakib100@gmail.com>
2024-09-23 21:58:41 +0600

    Setup

---
+++ firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8yofGTG2_JS2VG8L2N6uFa9Mz6rzBL4Y",
+  authDomain: "teachtrove-e43dd.firebaseapp.com",
+  projectId: "teachtrove-e43dd",
+  storageBucket: "teachtrove-e43dd.appspot.com",
+  messagingSenderId: "653755412168",
+  appId: "1:653755412168:web:c86c80beba357f27f9fffd",
+ 
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
